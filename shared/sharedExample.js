5819e3d53e678b64941f0110826319c1e4dd82a3
arulmurugan1982 <77025069+arulmurugan1982@users.noreply.github.com>
2022-04-07 13:30:40 -0500

    Update sharedExample.js

--- shared/sharedExample.js
+++ shared/sharedExample.js
@@ -3,13 +3,13 @@ const AWS = require('aws-sdk');
 
 exports.shared = async function(event, context) {
   try {
-      console.log("Entering Lambda FreightWise Sharing!")
+      console.log("Entering Lambda Sharing!")
 
   } catch(error) {
     return {
       statusCode: 400,
         headers: {},
-        body: JSON.stringify("FreightWise")
+        body: JSON.stringify("Test")
     }
   }
 }
